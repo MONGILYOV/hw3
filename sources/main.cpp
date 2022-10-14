176a736ca55b765c3f531d1ef9faa3d2c3af5831
Mongilyov Andrey <andrey1987697@gmail.com>
2022-10-14 23:53:45 +0300

    commit #4

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,8 @@
 // Copyright Mongilyov Andrey & Novikov Igor 2022
 
 #include <iostream>
+#include "../include/myFunctions.hpp"
 
 int main() {
-    std::cout << "Hello, World!";
+    printHelloWorld();
 }

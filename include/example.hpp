a1c9ac11da8e5e63164ac933c014e7680b148cf7
Mongilyov Andrey <andrey1987697@gmail.com>
2022-10-14 22:56:33 +0300

    commit #3

--- include/example.hpp
+++ include/example.hpp
@@ -2,7 +2,7 @@
 // Created by Андрей Монгилёв on 14.10.2022.
 //
 
-#ifndef WAYFIND_EXAMPLE_HPP
-#define WAYFIND_EXAMPLE_HPP
+#ifndef WAYFIND_EXAMPLE_HPP_
+#define WAYFIND_EXAMPLE_HPP_
 
-#endif //WAYFIND_EXAMPLE_HPP
+#endif //WAYFIND_EXAMPLE_HPP_
